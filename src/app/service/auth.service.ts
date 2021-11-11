bc579aafa138820cb3e74ed4c6086d1b05ea16e7
guilhermem0101 <guilherme.sou@hotmail.com>
2021-11-11 09:35:21 -0300

    atualização

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -12,9 +12,9 @@ export class AuthService {
   constructor(private http: HttpClient ) { }
 
   entrar(userLogin: UserLogin) : Observable <UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('https://toddes.herokuapp.com/usuarios/logar', userLogin)
   }
   cadastrar(user: User): Observable <User>{
-    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
+    return this.http.post<User>('https://toddes.herokuapp.com/usuarios/cadastrar', user)
   }
 }
