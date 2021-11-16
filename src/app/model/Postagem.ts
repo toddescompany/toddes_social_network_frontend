b66e6eec54ae0c50782a2395689488e6aa9aa1e3
renanimpacta <santana.renan@outlook.com>
2021-11-16 16:08:08 -0300

    Inclusão titulo, data e usuário na postagem

--- src/app/model/Postagem.ts
+++ src/app/model/Postagem.ts
@@ -5,7 +5,7 @@ export class Postagem{
     public id_postagem: number
     public tituloPostagem: string
     public texto_postagem: string
-    public date: Date
+    public data_postagem: Date
     public anonimo_postagem: boolean
     public fk_tema: Tema
     public fk_usuario: User
