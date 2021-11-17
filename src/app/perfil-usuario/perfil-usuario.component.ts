e9a77b0041bda1580cd818ad8965dbf3e2e1675d
guilhermem0101 <guilherme.sou@hotmail.com>
2021-11-17 19:12:29 -0300

    inserção de imagem(incompleto)

--- src/app/perfil-usuario/perfil-usuario.component.ts
+++ src/app/perfil-usuario/perfil-usuario.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
 import { User } from '../model/User';
 import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-perfil-usuario',
@@ -13,9 +17,12 @@ export class PerfilUsuarioComponent implements OnInit {
 
   user: User = new User()
   idUsuario = environment.id
-
+  listaPostagens: Postagem[]
+  tema: Tema = new Tema()
   constructor(
     private router: Router,
+    private postagemService: PostagemService,
+   
     private authservice: AuthService
   ) { }
 
@@ -25,11 +32,22 @@ export class PerfilUsuarioComponent implements OnInit {
       this.router.navigate(['/entrar'])
     }
     this.findByIdUser()
+   
+    this.getAllPostagens()
   }
 
   findByIdUser(){
     this.authservice.getByIdUser(this.idUsuario).subscribe((resp: User)=>{
       this.user = resp
     })
+  }
+
+
+
+  getAllPostagens(){
+    this.postagemService.getAllPostagem().subscribe((resp: Postagem[])=>{
+      this.listaPostagens = resp
+    })
+
   }
 }
