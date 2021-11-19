e5659ca58f0498a7be92e517be28275fc67e758c
argelcapela <argelcapeladossantos@gmail.com>
2021-11-18 21:15:09 -0300

    Re-alinhamento do layout do feed!

--- src/app/menu-esquerdo/menu-esquerdo.component.ts
+++ src/app/menu-esquerdo/menu-esquerdo.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { User } from '../model/User';
+import { AlertasService } from '../service/alertas.service';
+import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-menu-esquerdo',
@@ -7,11 +15,70 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./menu-esquerdo.component.css']
 })
 export class MenuEsquerdoComponent implements OnInit {
-  
+
+
   idUsuario = environment.id
-  constructor() { }
+  postagem = new Postagem()
+  listaTemas : Tema[]
+  tema: Tema = new Tema()
+  idTema: number
+  user = new User()
+
+  constructor(
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService,
+    private authservice: AuthService,
+    private alertas: AlertasService
+  ) { }
 
   ngOnInit(): void {
+
+    this.getAllTemas()
+    this.findByIdUser()
   }
+
+
+  // preparar modal
+  getAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=> {
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema)=>{
+      this.tema = resp
+    })
+  }
+
+  findByIdUser(){
+    this.authservice.getByIdUser(this.idUsuario).subscribe((resp: User)=>{
+      this.user = resp
+    })
+  }
+
+  // publicar postagem
+  publicarPostagem()
+  {
+
+  this.postagem.anonimo_postagem = false
+  this.tema.idTema = this.idTema
+  this.postagem.fk_tema = this.tema
+  this.user.id = this.idUsuario
+  this.postagem.fk_usuario = this.user
+
+
+  this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+    this.postagem = resp
+    this.alertas.showAlertSuccess('Postagem realizada com sucesso! O post será exibido em instantes!')
+    this.postagem = new Postagem()
+  })
+
+  this.router.navigate(['/feed'])
+}
+
+
+
 
 }
