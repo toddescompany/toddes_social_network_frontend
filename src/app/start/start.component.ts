2621339c4a585e99efb1473b7f6f0f4692032933
argelcapela <argelcapeladossantos@gmail.com>
2021-11-24 08:47:24 -0300

    Netlify não permite arquivos maiores que 4kb, o css start estava com 8, então dividi em dois arquivos, vou tentar agora!

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
-  styleUrls: ['./start.component.css']
+  styleUrls: ['./start.component.css','./start2.component.css']
 })
 export class StartComponent implements OnInit {
 
