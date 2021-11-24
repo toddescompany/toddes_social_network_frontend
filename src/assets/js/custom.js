05327fd449138da7eaa1f180a5cf26dfe3ec3e0c
argelcapela <argelcapeladossantos@gmail.com>
2021-11-23 22:24:07 -0300

    Layout responsive terminado no start!

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -154,10 +154,37 @@ if($(window).width() >= 992)
 
 btn_subir.click(function(){
     $('html,body').animate({scrollTop:0},1000);
-})
+});
 
 
-  });
+
+
+/* EXPANDIR IMAGEM AO CLICAR */
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("der");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+$(".expandirIMG").click(function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+});
+
+
 
 
 
